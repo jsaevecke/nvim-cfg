ba65a2177709d0ac0858d90a4a67d452db1199be
Julien Saevecke <julien.saevecke@kbst-gmbh.de>
2024-04-15 10:18:06 +0200

    add highlight on yank

--- lua/pixian/remap.lua
+++ lua/pixian/remap.lua
@@ -14,6 +14,14 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
     end,
 })
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
 vim.keymap.set("n", "<leader>l", function()
     require('lint').try_lint()
 end, { desc = "Lint current file" })
