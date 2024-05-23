619ad07840e5d5fbb350ba2b7eae4cd696ca6dbf
Julien Saevecke <julien.saevecke@kbst-gmbh.de>
2024-05-23 10:37:39 +0200

    update

--- lua/pixian/init.lua
+++ lua/pixian/init.lua
@@ -1,3 +1,14 @@
 require("pixian.remap")
 require("pixian.set")
 require("pixian.color")
+
+vim.filetype.add {
+    extension = {
+        zsh = "sh",
+        sh = "sh",
+    },
+    filename = {
+        [".zshrc"] = "sh",
+        [".zshenv"] = "sh",
+    },
+}
