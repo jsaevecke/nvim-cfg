400e39c87797312ea024dc5dd95abaf85594f872
Julien Saevecke <julien.saevecke@kbst-gmbh.de>
2024-09-18 08:18:20 +0200

    update config for new nvim version

--- lua/pixian/remap.lua
+++ lua/pixian/remap.lua
@@ -1,3 +1,5 @@
+Trouble = require("trouble")
+
 vim.g.mapleader = " "
 
 -- GENERAL
@@ -30,9 +32,9 @@ end, { desc = "Lint current file" })
 vim.keymap.set("n", "<leader>t", ":GoTestFunc<CR>", { desc = "Go Test Function" })
 
 -- TROUBLE
-vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
-vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
-vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
-vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
-vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
-vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+vim.keymap.set("n", "<leader>xx", function() Trouble.toggle("diagnostics") end)
+vim.keymap.set("n", "<leader>xw", function() Trouble.toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() Trouble.toggle("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() Trouble.toggle("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() Trouble.toggle("loclist") end)
+vim.keymap.set("n", "gR", function() Trouble.toggle("lsp_references") end)
